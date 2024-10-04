ffc27e764ae19db1934a8de7aec8fcdc7139779b
davy <144752667+davy2333@users.noreply.github.com>
2024-10-04 12:29:06 -0600

    agregue la clase pacienteDatos para manejar los procedimientos almacenados

---
+++ ClinicOrg/Controllers/PacientesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ClinicOrg.Models;
+using ClinicOrg.Data;
+
+
+namespace ClinicOrg.Controllers
+{
+    public class PacientesController : Controller
+    {
+        Paciente _paciente = new Paciente();
+        public IActionResult Listar()
+        {
+            //mostrara la lista de pacientes
+            var oLista = _paciente.
+            return View(oLista);
+        }
+
+
+        public IActionResult Guardar()
+        {
+            //metodo que solo devuelve la vista
+            return View();
+        }
+
+        public IActionResult Guardar()
+        {
+
+            
+            //metodo recibe el objeto para guardarlo en db
+            return View();
+        }
+    }
+}
---
+++ ClinicOrg/Data/PacienteDatos.cs
@@ -0,0 +1,45 @@
+﻿using ClinicOrg.Models;
+using System.Data.SqlClient;
+using System.Data;
+using ClinicOrg.Models.Datos;
+using Microsoft.AspNetCore.Identity;
+namespace ClinicOrg.Data
+
+
+{
+    public class PacienteDatos
+    {
+        public List<Paciente> Listar()
+        {
+            var oLista = new List<Paciente>();
+
+            var cn = new Conexion();
+
+            using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("listar_pacientes", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        oLista.Add(new Paciente()
+                        {
+                            Id = Convert.ToInt32(dr["ID"]),
+                            Nombre = dr["Nombre"].ToString(),
+                            Apellido = dr["Apellido"].ToString(),
+                            Sexo = dr["Sexo"].ToString(),
+                            Telefono = dr["Telefono"].ToString(),
+                            FechaNacimiento = Convert.ToDateTime(dr["fecha de nacimiento"]),
+                            Email = dr["Email"].ToString(),
+                        });
+                    }
+                }
+            }
+            return oLista;
+        }
+
+    }
+}
