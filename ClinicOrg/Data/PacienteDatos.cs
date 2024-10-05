ca72702b0566b810a76eb412fafb5dfc86d45675
davy <144752667+davy2333@users.noreply.github.com>
2024-10-05 10:36:08 -0600

    agregue el metodo obtener

--- ClinicOrg/Data/PacienteDatos.cs
+++ ClinicOrg/Data/PacienteDatos.cs
@@ -40,6 +40,37 @@ namespace ClinicOrg.Data
             }
             return oLista;
         }
+        public Paciente Obterner(int Personid)
+        {
+            var oPaciente = new Paciente();
+
+            var cn = new Conexion();
+
+            using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("Obtener_pacientes", conexion);
+                cmd.Parameters.AddWithValue("Personid", Personid);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+
+                        oPaciente.Id = Convert.ToInt32(dr["ID"]);
+                        oPaciente.Nombre = dr["Nombre"].ToString();
+                        oPaciente.Apellido = dr["Apellido"].ToString();
+                        oPaciente.Sexo = dr["Sexo"].ToString();
+                        oPaciente.Telefono = dr["Telefono"].ToString();
+                        oPaciente.FechaNacimiento = Convert.ToDateTime(dr["fecha de nacimiento"]);
+                        oPaciente.Email = dr["Email"].ToString();
+                        
+                    }
+                }
+            }
+            return oPaciente;
+        }
 
     }
 }
